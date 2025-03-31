358522af82477af1f5615870b2fc72364c6c7734
MackInSpace <amackey@lssu.edu>
2025-03-31 13:05:27 -0600

    updated the campsite info screen with comments and styles

--- nucampsite/screens/AboutScreen.js
+++ nucampsite/screens/AboutScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ScrollView, Text } from "react-native";
 import { Avatar, Card, ListItem } from "react-native-elements";
 import { PARTNERS } from "../shared/partners"; 
