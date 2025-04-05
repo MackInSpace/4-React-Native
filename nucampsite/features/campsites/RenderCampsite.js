48d72af30f28a2a17a705170f4b3e237bde3f547
MackInSpace <amackey@lssu.edu>
2025-04-05 10:02:34 -0600

    setting up modal in project

--- nucampsite/features/campsites/RenderCampsite.js
+++ nucampsite/features/campsites/RenderCampsite.js
@@ -16,23 +16,33 @@ const RenderCampsite = (props) => {
                                 fontSize: 20
                             }}
                         >
-                            {campsite.name}
+                            {styles.cardText}
                         </Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <Icon
-                    name={props.isFavorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() =>
-                        props.isFavorite
-                            ? console.log('Already set as a favorite')
-                            : props.markFavorite()
-                    }
-                />
+                <View style={styles.cardRow}>    
+                    <Icon
+                        name={props.isFavorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() =>
+                            props.isFavorite
+                                ? console.log('Already set as a favorite')
+                                : props.markFavorite()
+                        }
+                    />
+                    <Icon 
+                        name='pencil'
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={() => props.onPress()}
+                    />
+                </View>
             </Card>
         );
     }
@@ -44,6 +54,21 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
+    },
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
     }
 });
 
