eb45aacaa74d359705b236b13f339d1ef5afdf67
MackInSpace <amackey@lssu.edu>
2025-04-05 10:37:45 -0600

    task 2 completed

--- nucampsite/screens/CampsiteInfoScreen.js
+++ nucampsite/screens/CampsiteInfoScreen.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { useState } from 'react';
+import { Rating, Input } from 'react-native-elements';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
@@ -10,12 +11,39 @@ const CampsiteInfoScreen = ({ route }) => {
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
     const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+
+        console.log(newComment);
+        setShowModal(!showModal);
+        resetForm();
+        };
+
+    const resetForm = () => {
+        setRating(5);
+        setAuthor('');
+        setText('');
+    }
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Rating 
+                    startingValue={item.rating}
+                    imageSize={10}
+                    readonly
+                    style={{ alignItems: 'flex-start', paddingVertical: '5%' }}
+                />
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
@@ -54,11 +82,47 @@ const CampsiteInfoScreen = ({ route }) => {
                 onRequestClose={() => setShowModal(false)}
             >
                 <View style={styles.modal}>
+                    <Rating 
+                        showRating
+                        startingValue={rating}
+                        imageSize={40}
+                        onFinishRating={(rating) => setRating(rating)}
+                        style={{ paddingVertical: 10 }}
+                    />
                     <View style={{ margin: 10 }}>
+                        <TextInput
+                            placeholder='Your Name'
+                            leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                            leftIconContainerStyle={{ paddingRight: 10 }}
+                            value={author}
+                            onChangeText={(text) => setAuthor(text)}
+                            style={styles.input}
+                        >   
+                        </TextInput>
+                        <TextInput
+                            placeholder='Write a Comment'
+                            leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                            leftIconContainerStyle={{ paddingRight: 10 }}
+                            value={text}
+                            onChangeText={(text) => setText(text)}
+                            style={styles.input}
+                        >
+                        </TextInput>
+                        <Button 
+                            title='Submit' 
+                            color='#5637DD'
+                            onPress={() => {
+                                handleSubmit();
+                                resetForm();
+                            }}
+                        />
                         <Button 
                             tile='Cancel'
                             color='#808080'
-                            onPress={() => setShowModal(!showModal)}
+                            onPress={() => {
+                                setShowModal(!showModal)
+                                resetForm()
+                            }}
                         />
                     </View>   
                 </View>
@@ -85,6 +149,12 @@ const styles = StyleSheet.create({
     modal: {
         justifyContent: 'center',
         margin: 20
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        marginBottom: 10
     }
 });
 
