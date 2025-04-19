f4f8f1b6f7ea92ca514733aeca8334ce406dd955
MackInSpace <amackey@lssu.edu>
2025-04-19 08:52:04 -0600

    added gallery selections

--- nucampsite/screens/LoginScreen.js
+++ nucampsite/screens/LoginScreen.js
@@ -163,6 +163,24 @@ const RegisterTab = () => {
         }
     };
 
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (mediaLibraryPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            
+            if (capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                await processImage(capturedImage.assets[0].uri);
+            }
+        } else {
+            console.log('Media library permission not granted');
+        }
+    };
+
     return (
         <ScrollView>
             <View style={styles.container}>
@@ -173,6 +191,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title='Camera' onPress={getImageFromCamera} />
+                    <Button title='Gallery' onPress={getImageFromGallery} />
                 </View>
                 <Input
                     placeholder='Username'
